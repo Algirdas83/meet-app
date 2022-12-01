01bcb295a5c1ff506fdc967b1ba552812c52f86c
Algirdas83 <algirdasliutkus@gmail.com>
2022-12-01 21:45:07 +0200

    added new page new componets

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-            <Link to="/"> All Meetups</Link>
+            <Link className={classes.active} to="/">
+              {" "}
+              All Meetups
+            </Link>
           </li>
           <li>
             <Link to="/new-meetUp">Add New Meetup</Link>
           </li>
           <li>
-            <Link to="/favorites"> Favorites</Link>
+            <Link to="/favorites"> My Favorites</Link>
           </li>
         </ul>
       </nav>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,39 @@
+import MeetupList from "../components/meetUps/MeetupList";
+
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is the first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Quite_summer_evening_in_the_port_city_Klaipeda.jpg/1920px-Quite_summer_evening_in_the_port_city_Klaipeda.jpg",
+    address: "MeetupsStreet 5, 12345 Klaipeda City",
+    description: "This is first amazing meetup, jeeeei",
+  },
+  {
+    id: "m2",
+    title: "This is the first meetup 2",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2a/Vilnius_old_town_by_Augustas_Didzgalvis.jpg",
+    address: "MeetupsStreet 5, 12345 Vilnius City",
+    description: "This is first amazing meetup, jeeeei",
+  },
+  {
+    id: "m3",
+    title: "This is the first meetup 3",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Vytautas_the_Great_Bridge_from_hill%2C_Kaunas%2C_Lithuania_-_Diliff.jpg/1920px-Vytautas_the_Great_Bridge_from_hill%2C_Kaunas%2C_Lithuania_-_Diliff.jpg",
+    address: "MeetupsStreet 5, 12345 Kaunas City",
+    description: "This is first amazing meetup, jeeeei",
+  },
+];
+
 const AllMeetupsPage = () => {
-  return <div> All Meetups Page</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={DUMMY_DATA} />
+    </section>
+  );
 };
 
 export default AllMeetupsPage;
