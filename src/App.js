3d0ab16494809f4bbbf1e47dda7aaeba007c5844
Algirdas83 <algirdasliutkus@gmail.com>
2022-12-01 17:36:46 +0200

    Added router dom navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
+
 function App() {
-  return <div className="App m-3"></div>;
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetUp" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
